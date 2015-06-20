ba7b466a869087078e195780f832090c379e57b5
Simon Koch <agorilla@me.com>
2015-06-20 22:49:20 +0200

    Location section with Theaterhaus
    - Added 8 images from http://www.theaterhaus-koeln.de/fotogalerie/
    - Performance optimzied images with 30% quality and ImageOptim
    - Modified location description
    - Added public transportation info text
    - Re-integrated location map scripts
    - Reduced desktop footer top space slightly

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 (function () {
 	'use strict';
 
+	// Load CSS async. Script adapted from https://github.com/filamentgroup/loadCSS/blob/master/loadCSS.js
+	function loadCSS (href, before, media) {
+		var styleSheet  = window.document.createElement('link');
+		var reference = before || window.document.getElementsByTagName('script')[0];
+
+		styleSheet.rel = "stylesheet";
+		styleSheet.href = href;
+		styleSheet.media = "only x";
+		reference.parentNode.insertBefore(styleSheet, reference);
+
+		setTimeout(
+			function () {
+				styleSheet.media = media || 'all';
+			}
+		);
+	}
+
 	/**
 	 * Gallery
 	 */
@@ -16,6 +33,26 @@
 			$('.gallery__image__image img').css('opacity', 1);
 		}, 200);
 	});
+
+	/*
+	 * Location map
+	 */
+
+	loadCSS('http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css');
+
+	var map = L.map('map', {
+		center: [50.95109, 6.91963],
+		zoom: 13,
+		scrollWheelZoom: false,
+		doubleClickZoom: false
+	});
+
+	L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+			attribution: '&copy;<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy;<a href="http://developer.mapquest.com/web/products/open/map">MapQuest</a>',
+			maxZoom: 18,
+		}).addTo(map);
+
+		L.marker([50.9504364,6.9215663]).addTo(map);
 
 	/*! A fix for the iOS orientationchange zoom bug.
 	 Script by @scottjehl, rebound by @wilto.
