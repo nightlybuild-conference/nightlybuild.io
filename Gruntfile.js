69965453963919b518eecefd3e084807cd8b4171
Sebastian Golasch <public@asciidisco.com>
2015-06-26 13:57:10 +0200

    fix(photoswipe): Fix photoswipe feature by adding the template to the copy & min tasks in the gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,7 @@ module.exports = function(grunt) {
           {expand: true, src: ['index.html'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['last-minute.html'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['codeofconduct.html'], dest: 'dist/', filter: 'isFile'},
+          {expand: true, src: ['templates/photoswipe.html'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['robots.txt'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['conference.ics'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['CNAME'], dest: 'dist/', filter: 'isFile'},
@@ -89,7 +90,8 @@ module.exports = function(grunt) {
         files: {
           'dist/index.html': 'dist/index.html',
           'dist/last-minute.html': 'dist/last-minute.html',
-          'dist/codeofconduct.html': 'dist/codeofconduct.html'
+          'dist/codeofconduct.html': 'dist/codeofconduct.html',
+          'dist/templates/photoswipe.html': 'dist/templates/photoswipe.html'
         }
       }
     },
